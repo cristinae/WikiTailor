cada2e98fbddd3d918084fbcd732202e7926e325
Alberto Barron <albarron@gmail.com>
2017-01-26 17:47:54 +0300

    length factor for en-ar and ar-en ready

--- src/main/java/cat/lump/ir/sim/cl/len/LengthFactors.java
+++ src/main/java/cat/lump/ir/sim/cl/len/LengthFactors.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 /**It includes the default values for a bunch of language pairs, namely:
  * <ul>
+ * <li> ar-en
  * <li> cz-en
- * <li> de-en	      
+ * <li> de-en
+ * <li> en-ar	      
  * <li> en-cz
  * <li> en-de
  * <li> en-es
@@ -17,7 +19,8 @@ import java.util.Map;
  * <li> ru-en
  * </ul>
  * 
- * The parameters were estimated by txell on different corpora, including:
+ * The parameters were estimated by txell and Cristina (ar-related) on 
+ * different corpora, including:
  * 
  *  <ul> 
  *  <li> commoncrawl.wmt2013  
@@ -47,43 +50,45 @@ public class LengthFactors {
 	/**Stores the default mean and sd values for different language pairs.
 	 * The values are only for test purposes. They should be confirmed */
 	private static final double means_sd[][] = {
-	      {1.085 , 0.273},	//cz-en
-	      {0.961 , 0.463},	//de-en	      
-	      {0.972 , 0.245},	//en-cz
-	      {1.176 , 0.926},	//en-de
-	      {1.133 , 0.415},	//en-es
-	      {1.158 , 0.411},	//en-fr
-	      {1.157 , 0.678},	//en-ru	      	      
-	      {0.926 , 0.441},	//es-en
-	      {0.914 , 0.313},	//fr-en
-	      {1.069 , 0.668},	//ru-en	    
-	      {1.157 , 0.235},  //es-eu  //From Barron-Cedeno, A., Rosso, P., Agirre, E., and Labaka, G. 
-	      							 // Plagiarism Detection across Distant Language Pairs. 
-	      							 // In Huang and Jurafsky (2010). 
-	      							 // TODO Ask Gorka and recalculate with new corpora
-	      {1.056 , 0.545},  //en-eu  // TODO idem
-	      {1.005 , 0.087},	//ca-es  // From El Periodico
-	      {1.002 , 0.094},	//es-ca  // FIXME It seems it should be closer to the inverse ca-es
-	    };	
+	  {0.904 , 0.371},  //ar-en
+    {1.085 , 0.273},	//cz-en
+    {0.961 , 0.463},	//de-en
+    {1.229 , 0.591},  //en-ar
+    {0.972 , 0.245},	//en-cz
+    {1.176 , 0.926},	//en-de
+    {1.133 , 0.415},	//en-es
+    {1.158 , 0.411},	//en-fr
+    {1.157 , 0.678},	//en-ru	      	      
+    {0.926 , 0.441},	//es-en
+    {0.914 , 0.313},	//fr-en
+    {1.069 , 0.668},	//ru-en	
+    // From Barron-Cedeno, et al. Plagiarism Detection across Distant Language Pairs.  
+    {1.157 , 0.235},  //es-eu  
+    {1.056 , 0.545},  //en-eu  // TODO idem
+    {1.005 , 0.087},	//ca-es  // From El Periodico
+    {1.002 , 0.094},	//es-ca  // FIXME It seems it should be closer to the inverse ca-es
+  };	
 	
 	/**Stores the index to access the different default values */
 	private static final Map<String, Integer> index = new HashMap<String, Integer>(){
 		private static final long serialVersionUID = 1L;
 		{
-			put("cz-en", 0);
-			put("de-en", 1);
-			put("en-cz", 2);
-			put("en-de", 3);
-			put("en-es", 4);
-			put("en-fr", 5);
-			put("en-ru", 6);
-			put("es-en", 7);
-			put("fr-en", 8);
-			put("ru-en", 9);
-			put("es-eu", 10);
-			put("en-eu", 11);
-			put("ca-es", 12);
-			put("es-ca", 13);
+		  put("ar-en", 0);
+			put("cz-en", 1);
+			put("de-en", 2);
+			put("en-ar", 3);
+			put("en-cz", 4);
+			put("en-de", 5);
+			put("en-es", 6);
+			put("en-fr", 7);
+			put("en-ru", 8);
+			put("es-en", 9);
+			put("fr-en", 10);
+			put("ru-en", 11);
+			put("es-eu", 12);
+			put("en-eu", 13);
+			put("ca-es", 14);
+			put("es-ca", 15);
 		}};
      	
 	/**
