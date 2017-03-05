504f268a017b13a3f2bbc2d059da0750dc2870dc
Alberto Barron <albarron@gmail.com>
2017-03-05 14:33:36 +0300

    a simple class to read some index info

---
+++ src/main/java/cat/lump/ir/lucene/index/LuceneIndexExplorer.java
@@ -0,0 +1,44 @@
+package cat.lump.ir.lucene.index;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.TermFreqVector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class LuceneIndexExplorer {
+  
+  private static final String indexPath = "/data/alt/corpora/semeval2017/task1/clesa/tinyindex/ar";
+  
+  public static void main(String[] args) throws IOException {
+    Directory dir = FSDirectory.open(new File(indexPath));
+    IndexReader reader = IndexReader.open(dir);
+    for (int i=0; i<reader.maxDoc(); i++) {
+      if (reader.isDeleted(i))
+          continue;
+
+      Document doc = reader.document(i);
+      String docId = doc.get("filename");
+      
+      System.out.println(docId);
+      TermFreqVector x = reader.getTermFreqVector(i, LuceneIndexerWT.CONTENTS_NAME);
+      for (String s : x.getTerms()) {
+        System.out.print(s + " ");
+      }
+      System.out.println();
+//      doc.get
+//      System.out.println(doc.getFields());
+//      System.out.println(doc.get(LuceneIndexerWT.CONTENTS_NAME));//"fullpath"));
+
+//      String docId = doc.get(LuceneIndexerWT.CONTENTS_NAME);
+//      doc.get("")
+//      System.out.println(docId);
+//      System.out.println(contents.toString());
+      // do something with docId here...
+  }
+  }
+
+}
